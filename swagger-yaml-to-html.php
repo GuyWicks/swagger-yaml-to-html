45f948a1726be99439c584bcb4563e6d869a5efc
GuyWicks <guy.wicks@gmail.com>
2019-01-23 16:23:25 +0000

    Update swagger-ui version
    I dont use the php version myself, but updated for consistency.

--- swagger-yaml-to-html.php
+++ swagger-yaml-to-html.php
@@ -10,7 +10,7 @@ exit;
 	<meta charset="UTF-8">
 	<title>Swagger UI</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Code+Pro:300,600|Titillium+Web:400,600,700" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.2.2/swagger-ui.css" >
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.20.5/swagger-ui.css" >
 	<style>
 		html
 		{
@@ -35,8 +35,8 @@ exit;
 
 <div id="swagger-ui"></div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.2.2/swagger-ui-bundle.js"> </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.2.2/swagger-ui-standalone-preset.js"> </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.20.5/swagger-ui-bundle.js"> </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.20.5/swagger-ui-standalone-preset.js"> </script>
 <script>
     window.onload = function() {
 
